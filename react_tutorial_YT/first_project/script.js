174fae90dc759b113cb8edbd4725541152c1ed96
reinhardjakesacay <reinhardjakesacayforwork@gmail.com>
2023-12-11 22:35:45 +0800

    react practice

--- react_tutorial_YT/first_project/script.js
+++ react_tutorial_YT/first_project/script.js
@@ -1,1 +1,13 @@
-ReactDOM.render(<h1>Hello, Reinhard Jake</h1>, document.getElementById("root"))
+const page = <div>
+    <h1>
+        This is a header 1
+    </h1>
+    <p>
+        This is a paragraph
+    </p>
+</div>
+
+
+ReactDOM.render(page,
+    document.getElementById("root")
+)
