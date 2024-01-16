c1b3c19a0800b097ebfb4dd9d942dbf4a4332868
reinhardjakesacay <reinhardjakesacayforwork@gmail.com>
2024-01-16 21:07:11 +0800

    search not woorking at the moment

--- movie_app/script.js
+++ movie_app/script.js
@@ -6,12 +6,56 @@ const main = document.getElementById("section");
 const form = document.getElementById("form");
 const search = document.getElementById("query");
 
-    returnMovies()
+    returnMovies(APILINK);
 function returnMovies(url){
     fetch(url).then(res => res.json()).then(function(data){
         console.log(data);
         data.results.forEach(element => {
-            const div = document.createElement('div');
+            // create div tags for the needed elements in movie app
+            const div_card = document.createElement('div');
+            div_card.setAttribute('class', 'card');
+
+            const div_row = document.createElement('div');
+            div_row.setAttribute('class', 'row');
+
+            const div_column = document.createElement('div');
+            div_column.setAttribute('class', 'column');
+
+            const image = document.createElement('img');
+            image.setAttribute('class', 'thumbnail');
+            image.setAttribute('id', 'image');
+
+            const title = document.createElement('h3');
+            title.setAttribute('class', 'title');
+
+            const center = document.createElement('center');
+            // get the title and img using the imgPath and .title query
+            // title.innerHTML = '${element.title}';
+            title.innerHTML = `${element.title}`;
+            image.src = IMG_PATH + element.poster_path;
+            // appendChild is when you format it according to the html structure that had been comment out 
+            center.appendChild(image);
+            div_card.appendChild(center);
+            div_card.appendChild(title);
+            div_column.appendChild(div_card);
+            div_row.appendChild(div_column);
+
+
+            // put all those element into the section tag in the html
+            main.appendChild(div_row);
+
         });
     });
 }
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    main.innerHTML = '';
+
+    const searchItem = search.value;
+
+    if (searchItem) {
+        returnMovies(SEARCHAPI + searchItem);
+        search.value = "";
+    }
+});
